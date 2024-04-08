2cfd33a48fdd249002be167fd333f54db3eb6138
Ivsmcrew <Ivsmcrew@gmail.com>
2024-04-08 22:08:04 +0300

    feat: add interactivity for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Card.module.css'
 
 function Card({data}) {
+  const [isActive, setIsActive] = useState(false);
+
+  function clickHandler() {
+    setIsActive(!isActive)
+  }
+
   return (
-    <div className={styles.card}>
+    <div 
+      className={isActive ? `${styles.card} ${styles.card_active}` : styles.card} 
+      onClick={() => {clickHandler()}}
+    >
       <h2 className={styles.card__title}>{data.title}</h2>
       <p className={styles.card__desc}>{data.desc}</p>
     </div>
